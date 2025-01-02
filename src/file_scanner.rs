115eb75809296a3031c2c5e8018b74d0920dcc21
SuccessfullyFailed <60329202+RenzoGerritzen@users.noreply.github.com>
2025-01-03 00:59:15 +0100

    Make FileScanner more efficient

--- src/file_scanner.rs
+++ src/file_scanner.rs
@@ -1,10 +1,15 @@
 use crate::{ FileRef, SEPARATOR };
-use std::error::Error;
 
 
 
 pub type ResultFilter = Box<dyn Fn(&FileRef) -> bool>;
-struct FileScannerCursor { dir:FileRef, last_parsed_file:Option<FileRef>, parsed_self: bool }
+struct FileScannerCursor {
+	parsed_self:bool,
+	current_dir:Option<FileRef>,
+	entries_in_current_dir:Option<Vec<FileRef>>,
+	previous_entry_index:Option<usize>,
+	entries_cache:Vec<(FileRef, Vec<FileRef>)>
+}
 
 
 
@@ -33,7 +38,13 @@ impl FileScanner {
 			results_filter: Box::new(|_| true),
 			recurse_filter: Box::new(|_| false),
 
-			cursor: FileScannerCursor { dir: source_dir, last_parsed_file: None, parsed_self: false }
+			cursor: FileScannerCursor {
+				parsed_self: false,
+				current_dir: None,
+				entries_in_current_dir: None,
+				previous_entry_index: None,
+				entries_cache: Vec::new()
+			}
 		}
 	}
 
@@ -78,75 +89,122 @@ impl FileScanner {
 	/* USAGE METHODS */
 
 	/// Find the next matching file based on the cursor.
-	fn find_next_file_at_cursor(&mut self) -> Result<FileRef, Box<dyn Error>> {
+	fn find_next_at_cursor(&mut self) -> Option<FileRef> {
 		
-		// Try to find file at cursor.
-		if let Ok(entry) = if !self.cursor.parsed_self && self.include_self && self.source_dir.exists() { Ok(self.source_dir.clone()) } else { self.find_next_file_at(&self.cursor.dir, &self.cursor.last_parsed_file) } {
-			if entry.is_dir() && (self.recurse_filter)(&entry) {
-				self.cursor.dir = entry.clone();
-				self.cursor.last_parsed_file = None;
-			} else {
-				self.cursor.dir = entry.parent_dir()?;
-				self.cursor.last_parsed_file = Some(entry.clone());
-			}
+		// Parse self if necessary.
+		if !self.cursor.parsed_self {
 			self.cursor.parsed_self = true;
-			return Ok(entry);
+			if self.include_self && self.source_dir.exists() && (self.results_filter)(&self.source_dir) {
+				return Some(self.source_dir.clone());
+			}
 		}
 
-		// Could not find any files.
-		return Err("Could not find file.".into())
+		// If no current dir exists, start at root dir.
+		if self.cursor.current_dir.is_none() {
+			self.cursor.current_dir = Some(self.source_dir.clone());
+		}
+
+		// Try to find the next item in the current dir.
+		if let Some((entry_index, entry)) = self.find_next_in_current_dir() {
+			self.cursor.previous_entry_index = Some(entry_index);
+			return Some(entry);
+		}
+
+		// If not found in current dir, try to recurse into sub-dir.
+		if let Some(current_dir) = self.cursor.current_dir.clone() {
+			if let Some(sub_dir) = self.entries_in_dir(&current_dir).to_owned().iter().filter(|entry| entry.is_dir() && (self.recurse_filter)(*entry)).next() {
+				if let Some(entry) = self.find_in_new_dir(sub_dir.clone()) {
+					return Some(entry);
+				}
+			}
+		}
+
+		// If not found in any sub-dirs, keep moving to parent dirs while keeping inside the source dir.
+		if let Some(mut current_dir) = self.cursor.current_dir.clone() {
+			if let Ok(mut parent_dir) = current_dir.parent_dir() {
+				while parent_dir.contains(&self.source_dir.path()) {
+
+					// Find new sub-dirs in the parent dir, below the current dir.
+					let parent_dir_sub_dirs:Vec<FileRef> = parent_dir.list_dirs();
+					if let Some(own_index_in_parent_dir) = parent_dir_sub_dirs.iter().position(|dir| dir == &current_dir) {
+						if let Some(sub_dir) = parent_dir_sub_dirs[own_index_in_parent_dir + 1..].iter().filter(|dir| (self.recurse_filter)(&dir)).next() {
+							if let Some(entry) = self.find_in_new_dir(sub_dir.clone()) {
+								return Some(entry);
+							}
+						}
+					} else {
+						eprintln!("FileRef scanner Could not recurse back to parent of '{current_dir}'. Dir does not seem to exist anymore.");
+					}
+
+					// Move to parent dir.
+					if let Ok(parent_parent_dir) = parent_dir.parent_dir() {
+						current_dir = parent_dir.clone();
+						parent_dir = parent_parent_dir;
+					}
+				}
+			}
+		}
+		
+		// No results.
+		None
 	}
 
-	/// Find the next file from a specific directory.
-	fn find_next_file_at(&self, dir:&FileRef, last_parsed_file:&Option<FileRef>) -> Result<FileRef, Box<dyn Error>> {
+	/// Move the cursor to a new place and find the next file there.
+	fn find_in_new_dir(&mut self, new_dir:FileRef) -> Option<FileRef> {
+		self.cursor.current_dir = Some(new_dir);
+		self.cursor.previous_entry_index = None;
+		self.cursor.entries_in_current_dir = None;
+		self.find_next_at_cursor()
+	}
+
+	/// Try to find the next entry in the current dir.
+	fn find_next_in_current_dir(&mut self) -> Option<(usize, FileRef)> {
+
+		// List and store entries in current dir.
+		if self.cursor.entries_in_current_dir.is_none() {
+			if let Some(current_dir) = self.cursor.current_dir.clone() {
+				self.cursor.entries_in_current_dir = Some(self.entries_in_dir(&current_dir).to_owned());
+			}
+		}
+
+		// Find suitable entries in current dir.
+		if let Some(current_dir_entries) = &self.cursor.entries_in_current_dir {
+			let start_index:usize = self.cursor.previous_entry_index.map(|index| index + 1).unwrap_or(0);
+			for (entry_index, entry) in current_dir_entries.iter().enumerate().skip(start_index) {
+				if self.entry_matches_filter(entry) {
+					return Some((entry_index, entry.clone()));
+				}
+			}
+		}
+
+		// None found.
+		None
+	}
+
+	/// Check if a file or dir matches the filters.
+	fn entry_matches_filter(&self, entry:&FileRef) -> bool {
+		(if entry.is_dir() { self.include_dirs } else { self.include_files }) && (self.results_filter)(entry)
+	}
+
+	/// List the entries in a specific dir.
+	fn entries_in_dir(&mut self, dir:&FileRef) -> &Vec<FileRef> {
 		use std::fs::read_dir;
 
-		// Collect entries.
-		let mut entries:Vec<FileRef> = Vec::new();
-		if let Ok(results) = read_dir(dir.path()) {
-			for path in results.flatten() {
-				entries.push(FileRef::new(&path.path().display().to_string()));
-			}
-		}
-		entries.sort_by(|a, b| b.is_dir().cmp(&a.is_dir()));
-		let start_entry_index:usize = last_parsed_file.as_ref().map(|skipping_entry| entries.iter().position(|entry| entry == skipping_entry).map(|index| index + 1).unwrap_or_default()).unwrap_or_default();
-		entries = entries[start_entry_index..].to_vec();
-
-		// Iterate through entries.
-		for entry in &entries {
-			let is_dir:bool = entry.is_dir();
-
-			// Try to match result.
-			if ((is_dir && self.include_dirs) || (!is_dir && self.include_files)) && (self.results_filter)(entry) {
-				return Ok(entry.clone());
-			}
-
-			// Recurse into dirs.
-			if is_dir && (self.recurse_filter)(entry) {
-				if let Ok(file) = self.find_next_file_at(entry, &None) {
-					return Ok(file);
-				}
-			}
+		// Find in cache.
+		if let Some(cache_index) = self.cursor.entries_cache.iter().position(|(cache_dir, _)| cache_dir == dir) {
+			return &self.cursor.entries_cache[cache_index].1;
 		}
 
-		// Could not find in any files in this dir, try to find one in parent dir if that is in temp_file dir.
-		if let Ok(parent_dir) = dir.parent_dir() {
-			if parent_dir.contains(self.source_dir.path()) {
-				if let Ok(found) = self.find_next_file_at(&parent_dir, &Some(dir.clone())) {
-					return Ok(found);
-				}
-			}
-		}
-		
-		// Could not find any files.
-		return Err("Could not find file.".into())
-
+		// List entries in actual folder and store in cache.
+		let entries:Vec<FileRef> = read_dir(dir.path()).map(|results| results.flatten().map(|dir_entry| FileRef::new(dir_entry.path().to_str().unwrap())).collect::<Vec<FileRef>>()).unwrap_or_default();
+		self.cursor.entries_cache.push((dir.clone(), entries));
+		&self.cursor.entries_cache.last().unwrap().1
 	}
 }
 impl Iterator for FileScanner {
 	type Item = FileRef;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		self.find_next_file_at_cursor().map(|result| Some(result)).unwrap_or_default()
+		self.find_next_at_cursor()
 	}
 }
